098c726c24adae663e5e99d7fa5cb9d32b4890e4
Fuxx-1 <583742849@qq.com>
2021-03-07 18:25:02 +0800

    周日 2-21-03

---
+++ 代码上传至Github.bat
@@ -0,0 +1,11 @@
+chcp 65001
+call cls
+@echo off
+echo -----按任意键开始-----
+pause
+cd  %~dp0
+call git add --all
+call git commit -m "%date:~0,4%-%date:~5,2%-%date:~8,2%"
+call git push origin main
+echo -----命令执行结束-----
+pause
