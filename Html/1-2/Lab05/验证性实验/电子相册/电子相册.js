c883bc20429b3801e2f9d5f9a2cbf5dd3fc5cbb8
Fuxx-1 <583742849@qq.com>
2021-05-26 23:39:51 +0800

    2021-05-26

--- Html/1-2/Lab05/验证性实验/电子相册/电子相册.js
+++ Html/1-2/Lab05/验证性实验/电子相册/电子相册.js
@@ -4,27 +4,27 @@ $(function () {
         data: {
             Info: [
                 {
-                    src: "../images/1.jpg",
+                    src: "../../images/1.jpg",
                     Info: "海底"
                 },
                 {
-                    src: "../images/2.jpg",
+                    src: "../../images/2.jpg",
                     Info: "花园"
                 },
                 {
-                    src: "../images/3.jpg",
+                    src: "../../images/3.jpg",
                     Info: "雪域"
                 },
                 {
-                    src: "../images/10.jpg",
+                    src: "../../images/10.jpg",
                     Info: "海港"
                 },
                 {
-                    src: "../images/11.jpg",
+                    src: "../../images/11.jpg",
                     Info: "原野"
                 },
                 {
-                    src: "../images/12.jpg",
+                    src: "../../images/12.jpg",
                     Info: "大道"
                 },
                 
